279b5174b88332cd1fada76e98a108feeb4a56fd
eman obeidat <amoonobeidat43@gmail.com>
2023-05-07 19:56:37 +0300

    edit

--- DataStructure/insertShift.py
+++ DataStructure/insertShift.py
@@ -1,13 +1,12 @@
 arr1=[1,2,3,4,5,6,7]
 def insertShiftArray(arr, val):
     n = len(arr)
-    if n%2==0:
+    if n % 2==0:
       mid = n // 2
-      arr=arr.insert(mid,val)
+      arr.insert(mid,val)
       print(arr)
     else:
-        mid = n // 2
-        int(mid)
+        mid =int(n//2)
         arr.insert(mid+1, val)
         print(arr)
 
