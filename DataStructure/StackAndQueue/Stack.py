7f40d65ea2680cf20c15706ab1a5f81676a7bbe4
eman obeidat <amoonobeidat43@gmail.com>
2023-05-22 07:37:26 +0000

    update cc7

--- DataStructure/StackAndQueue/Stack.py
+++ DataStructure/StackAndQueue/Stack.py
@@ -6,7 +6,12 @@ class Node :
 class Stack:
     def __init__(self,top=None):
         self.top = top
-# Push method will create new node and add it to the top of stack
+        """
+        Pushes a value onto the top of the stack.
+
+        Args:
+            value: The value to be pushed onto the stack.
+        """
     def push(self,value):
         node=Node(value)
         if self.top is None:
@@ -14,29 +19,45 @@ class Stack:
         else:
             node.next=self.top
             self.top=node
-# Pop method remove the top value from stack
-    def pop(self):
-        if self.top is None:
-            raise EmptyError("Stack is empty!")
-        else:
-            temp=self.top
-            self.top=temp.next
-            temp.next=None
-        return temp.value
-# Peek method return the top value
-    def peek(self):
-        if self.top is None:
-            raise EmptyError("Stack is empty!")
-        else:
-            return self.top.value
+        """
+        Removes and returns the value from the top of the stack.
+
+        Returns:
+            The value from the top of the stack.
+        """
+        def pop(self):
+            if self.top is None:
+                raise EmptyError("Stack is empty!")
+            else:
+                temp=self.top
+                self.top=temp.next
+                temp.next=None
+            return temp.value
+        """
+        Returns the value from the top of the stack without removing it.
+
+        Returns:
+            The value from the top of the stack.
+
+        Raises:
+            Exception: If the stack is empty.
+        """
+        def peek(self):
+            if self.top is None:
+                raise EmptyError("Stack is empty!")
+            else:
+                return self.top.value
 # check if the stack is empty return true otherwise false  
     def is_empty(self):
         if self.top is None:
             return True
         else:
             return False
-        
-# This function will return the values of stack as string
+
+            
+        """   
+        A string representation of the stack, showing its elements from top to bottom.
+        """
     def __str__(self):
         current=self.top
         string=""
