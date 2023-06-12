b1bc26e632759041307777e4220e83468de6d315
eman obeidat <amoonobeidat43@gmail.com>
2023-06-12 09:57:20 +0000

    CC13 test

---
+++ DataStructure/tests/test_brackets.py
@@ -0,0 +1,35 @@
+from StackAndQueue.CC13 import validate_brackets
+
+
+def test_valid_brackets():
+    assert validate_brackets("{}") is True
+    assert validate_brackets("{}(){}") is True
+    assert validate_brackets("()[[Extra Characters]]") is True
+    assert validate_brackets("(){}[[]]") is True
+    assert validate_brackets("{}{Code}[Fellows](())") is True
+
+
+def test_invalid_brackets():
+    assert validate_brackets("[({}]") is False
+    assert validate_brackets("(](") is False
+    assert validate_brackets("{(})") is False
+
+
+def test_empty_string():
+    assert validate_brackets("") is True
+
+
+def test_single_bracket():
+    assert validate_brackets("{") is False
+    assert validate_brackets("}") is False
+    assert validate_brackets("[") is False
+    assert validate_brackets("]") is False
+    assert validate_brackets("(") is False
+    assert validate_brackets(")") is False
+
+
+def test_nested_brackets():
+    assert validate_brackets("[{()}]") is True
+    assert validate_brackets("{[()]}") is True
+    assert validate_brackets("{[}]") is False
+    assert validate_brackets("[{)]") is False
